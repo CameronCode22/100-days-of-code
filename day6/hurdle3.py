4c2da78f45f699bb7880e78a111fd0d47ce46daa
cameron Tait <jscamerontait@gmail.com>
2024-09-13 18:47:39 -0600

    forgot day

---
+++ day6/hurdle3.py
@@ -0,0 +1,29 @@
+def jump():
+    for i in range(1, 4):
+        turn_left()
+    move()
+    for i in range(1, 4):
+        turn_left()
+       
+    
+def find_max():
+    turn_left()
+    while wall_on_right():
+        if wall_in_front():
+            turn_left()
+        elif at_goal():
+            break
+        else:
+            move()
+    
+
+
+while not at_goal():
+    while front_is_clear():
+        move()
+
+    while wall_in_front():
+        find_max()
+        jump()        
+        
+        
