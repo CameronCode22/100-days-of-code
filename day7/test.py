355d004b32ee7c4515d333bf2deb6a8bccbc93af
cameron Tait <jscamerontait@gmail.com>
2024-10-03 15:48:08 -0600

    going well

--- day7/test.py
+++ day7/test.py
@@ -7,6 +7,57 @@ letters = [
     'u', 'v', 'w', 'x', 'y', 'z'
 ]
 
+HANGMANPICS = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+
 # List of 10 random words
 words_list = ['flub', 'zink', 'vopra', 'blanx', 'curil', 'jomed', 'spren', 'wilty', 'drost', 'gaxen']
 
@@ -21,35 +72,41 @@ print(' '.join(blank_word))
 print(blank_word)
 
 
-# TODO2 - Ask the user to guess a letter and assign their answer to a variable called guess, make guess lowercase
 completed_flag = False
 failed_flag = False
 num_guess = 0
-while not completed_flag:
 
-    letter = input("guess a letter").lower()
+while not completed_flag and not failed_flag:
 
-    # TODO3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it is, "Wrong" if it's not
+    letter = input("Guess a letter: ").lower()
+    correct_guess = False
 
-    for i in range(0,len(chosen_word)):
+    # Check if the guessed letter is in the chosen_word
+    for i in range(len(chosen_word)):
         if letter == chosen_word[i]:
             print("Correct Guess")
             blank_word[i] = letter
-            #update blank word
             print(' '.join(blank_word))
-            joined_blank = str(''.join(blank_word))
-            print(joined_blank)
-            num_guess += 1
-            print(num_guess)
-            if chosen_word == joined_blank:
-                completed_flag = True
-            elif num_guess == 10:
-                failed_flag = True
-                
-            
-            break
-    if completed_flag == True:
-        print("You won! congratulations")
-    elif failed_flag == True:
-        print("You lost! Try again!")
+            correct_guess = True
+
+    # Check if the word is fully guessed
+    if ''.join(blank_word) == chosen_word:
+        completed_flag = True
+
+    # Handle wrong guess
+    if not correct_guess:
+        num_guess += 1
+        print(f"Wrong guess. Number of wrong guesses: {num_guess}")
+        if num_guess == len(HANGMANPICS):
+            failed_flag = True
+
+    #Print hangman_art
+    print(HANGMANPICS[num_guess - 1] if num_guess > 0 else HANGMANPICS[0])
+
+# Game end
+if completed_flag:
+    print("You won! Congratulations!")
+elif failed_flag:
+    print("You lost! The word was:", chosen_word)
+
 
