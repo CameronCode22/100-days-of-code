d2f0cbaa0714d82d8085f0b595f0d855d95ed168
cameron Tait <jscamerontait@gmail.com>
2024-10-05 12:39:53 -0600

    Ceasar encrypt tool

---
+++ day8/caesar_cipher.py
@@ -0,0 +1,39 @@
+alphabet_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
+                 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+def encrypt(original_text, shift_amount):
+    count = 0
+    new_text = []
+    for letter_orig_text in original_text:
+        for index, letter_alphabet in enumerate(alphabet_list):
+            if letter_orig_text.lower() == letter_alphabet.lower():
+                new_position = (index + shift_amount) % 26
+                new_text.append(alphabet_list[new_position])
+                count += 1
+                print(new_text)
+    print(new_text)
+
+def decrypt(original_text, shift_amount):
+    count = 0
+    new_text = []
+    for letter_orig_text in original_text:
+        for index, letter_alphabet in enumerate(alphabet_list):
+            if letter_orig_text.lower() == letter_alphabet.lower():
+                new_position = (index - shift_amount) % 26
+                new_text.append(alphabet_list[new_position])
+                count += 1
+                print(new_text)
+    print(new_text)
+
+type = input("Type 'encrypt' or 'decrypt' ")
+message = input("Input your message!: ")
+shift_num = int(input("Enter shift numbers: "))
+
+if type.lower() == 'encrypt':
+    encrypt(message, shift_num)
+elif type.lower() == 'decrypt':
+    decrypt(message, shift_num)
+
+
+#encrypt("hello", 2) 
+#decrypt("JGNNQ", 2)
